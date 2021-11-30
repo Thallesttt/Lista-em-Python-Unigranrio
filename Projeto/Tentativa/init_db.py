e26d933de9b7769de10e19d80fa7beaf9bc50937
thallesttt <thallesttt.11@gmail.com>
2021-11-30 08:06:53 -0300

    reconstrução Banco de Dados Parte 2

--- Projeto/Tentativa/init_db.py
+++ Projeto/Tentativa/init_db.py
@@ -5,7 +5,7 @@ with open("schema.sql")as file:
     session.executescript(file.read())
 #with open("Schema Usuario.sql") as usuario:
 #    session.executescript(usuario.read())
-cursor = session.cursor() 
-cursor.execute("INSERT INTO TB_USUARIO (ID,NOME,SENHA) VALUES(1,'Thalles','Thalles')")
+#cursor = session.cursor() 
+#cursor.execute("INSERT INTO TB_USUARIO (ID,NOME,SENHA) VALUES(1,'Thalles','Thalles')")
 session.commit()
 session.close()
